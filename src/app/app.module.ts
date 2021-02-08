8d4505fee3c1ef1725d4e38c08c816e55f214fa9
maxszabi <maxszabi@gmail.com>
2021-02-08 11:55:36 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatNativeDateModule} from '@angular/material/core';
 
 import { AppComponent } from './app.component';
 import { AppProductComponent } from './app-product/app-product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule, 
+    MatProgressSpinnerModule,],
   declarations: [ AppComponent, AppProductComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  exports:      [ MatProgressSpinnerModule]
 })
 export class AppModule { }
